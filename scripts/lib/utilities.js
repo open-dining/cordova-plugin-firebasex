1bf7780a91c068942f2e1467617f3394fd1eefc4
Dave Alden <dave.alden@srt-marine.com>
2026-03-15 10:30:15 +0000

    (ios) Better resolution of app name new cordova-ios@8 Xcode project structure

--- scripts/lib/utilities.js
+++ scripts/lib/utilities.js
@@ -9,6 +9,9 @@ var _configXml, _pluginXml, _context, _pluginVariables;
 
 var Utilities = {};
 
+/** @constant {string} The expected app's Xcode name under `platforms/ios` for cordova-ios 8+. */
+var appNameCordova8Plus = "App";
+
 fs.ensureDirSync = function(dir){
     if(!fs.existsSync(dir)){
         dir.split(path.sep).reduce(function(currentPath, folder){
@@ -61,20 +64,59 @@ Utilities.writeJsonToXmlFile = function(jsonObj, filepath, parseOpts){
 };
 
 /**
+ * Determines whether the project is using cordova-ios 8+ by checking for the existence of the `platforms/ios/App` directory.
+ *
+ * @param {string} iosPlatformPath - Absolute path to the `platforms/ios` directory.
+ * @returns {boolean} True if cordova-ios 8+ layout is detected, false otherwise.
+ */
+Utilities.isCordovaIOS8Plus = function(iosPlatformPath) {
+    var appSubDirPath = path.join(iosPlatformPath, appNameCordova8Plus);
+    return fs.existsSync(appSubDirPath) && fs.statSync(appSubDirPath).isDirectory();
+};
+
+/**
+ * Returns the absolute path to the app subdirectory under `platforms/ios`.
+ * Supports both cordova-ios 8+ (`platforms/ios/App`) and legacy layouts (`platforms/ios/<AppName>`).
+ *
+ * @param {string} iosPlatformPath - Absolute path to the `platforms/ios` directory.
+ * @returns {string} Absolute path to the app subdirectory.
+ */
+Utilities.getAppSubDirPath = function(iosPlatformPath) {
+    var newPath = path.join(iosPlatformPath, appNameCordova8Plus);
+    if (fs.existsSync(newPath)) {
+        return newPath;
+    }
+    var legacyAppName = Utilities.parseConfigXml().widget.name._text.toString().trim();
+    return path.join(iosPlatformPath, legacyAppName);
+};
+
+/**
  * Used to get the name of the application from the xcodeCordovaProj directory path.
- * The xcodeCordovaProj directory path is defined in the locations property of the Cordova-iOS platform's API.
+ * For cordova-ios 8+, returns "App" directly without loading the cordova-ios module.
+ * For legacy cordova-ios versions, uses the cordova-ios API to resolve the project directory name.
+ * For non-iOS platforms, falls back to parsing config.xml.
  */
 Utilities.getAppName = function(){
-    if(_context && _context.opts.cordova.platforms.indexOf('ios') !== -1){
-        const projectRoot = _context.opts.projectRoot;
-        const platformPath = path.join(projectRoot, 'platforms', 'ios');
-        const cordova_ios = require('cordova-ios');
-        const iosProject = new cordova_ios('ios', platformPath);
-
-        return path.basename(iosProject.locations.xcodeCordovaProj);
+    if(!_context){
+        return Utilities.parseConfigXml().widget.name._text.toString().trim();
     }
-    // other platforms
-    return Utilities.parseConfigXml().widget.name._text.toString().trim();
+    if(_context.opts.cordova.platforms.indexOf('ios') === -1){
+        // other platforms
+        return Utilities.parseConfigXml().widget.name._text.toString().trim();
+    }
+
+    const projectRoot = _context.opts.projectRoot;
+    const platformPath = path.join(projectRoot, 'platforms', 'ios');
+
+    // Cordova-ios 8+: skip loading cordova-ios entirely by using the directory check
+    if(Utilities.isCordovaIOS8Plus(platformPath)){
+        return appNameCordova8Plus;
+    }
+
+    // Legacy cordova-ios: use the API to resolve the Xcode project directory name
+    const cordova_ios = require('cordova-ios');
+    const iosProject = new cordova_ios('ios', platformPath);
+    return path.basename(iosProject.locations.xcodeCordovaProj);
 };
 
 /**
